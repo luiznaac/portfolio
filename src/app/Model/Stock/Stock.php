b14c8b0d414562b482d0cde8e21013f43aeca8d9
luiznaac <luizagnern@hotmail.com>
2020-06-28 18:18:54 -0300

    Implementing stock_info load for given date

--- src/app/Model/Stock/Stock.php
+++ src/app/Model/Stock/Stock.php
@@ -3,6 +3,8 @@
 namespace App\Model\Stock;
 
 use App\Portfolio\API\AlphaVantageAPI;
+use Carbon\Carbon;
+use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Database\Eloquent\Model;
 
 /**
@@ -19,5 +21,14 @@ class Stock extends Model {
         $this->symbol = $symbol;
         $this->name = AlphaVantageAPI::getStockNameForSymbol($symbol);
         $this->save();
+    }
+
+    public function getStockInfos(): Collection {
+        return StockInfo::where('stock_id', $this->id)->orderBy('date')->get();
+    }
+
+    public function loadStockInfoForDate(Carbon $date): void {
+        $stock_info = new StockInfo();
+        $stock_info->store($this, $date);
     }
 }
--- src/tests/Model/StockTest.php
+++ src/tests/Model/StockTest.php
@@ -3,9 +3,25 @@
 namespace Tests\Model;
 
 use App\Model\Stock\Stock;
+use App\Model\Stock\StockInfo;
+use Carbon\Carbon;
 use Tests\TestCase;
 
 class StockTest extends TestCase {
+
+    public function testLoadStockInfoForDate_ShouldLoadAndStore(): void {
+        $stock = new Stock();
+        $stock->store('BOVA11');
+
+        $date = Carbon::createFromFormat('Y-m-d', '2020-06-26');
+        $stock->loadStockInfoForDate($date);
+
+        /** @var StockInfo $stock_info */
+        $stock_info = $stock->getStockInfos()->first();
+
+        $this->assertEquals($date->toDateString(), $stock_info->date);
+        $this->assertEquals(90.22, $stock_info->price);
+    }
 
     public function testStoreStock_StockNotFound_ShouldStoreNull(): void {
         $stock = new Stock();
