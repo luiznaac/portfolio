b4d8565a2fef0c747d5d9205dceb711c7efb6f62
luiznaac <luizagnern@hotmail.com>
2020-07-07 20:53:50 -0300

    Adding stock type

---
+++ src/app/Model/Stock/StockType.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Model\Stock;
+
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * App\Model\Stock\StockType
+ *
+ * @property int $id
+ * @property string $type
+ * @property string $description
+ */
+
+class StockType extends Model {
+
+}
