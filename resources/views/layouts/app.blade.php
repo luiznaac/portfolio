4fb9541513852537cb6638d7dab05551646b0730
luiznaac <luizagnern@hotmail.com>
2020-07-09 15:51:54 -0300

    Fixing assert to generate https

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,9 +3,9 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link rel="stylesheet" href="{{asset('site/style.css')}}">
-        <script src="{{asset('site/jquery.js')}}"></script>
-        <script src="{{asset('site/bootstrap.js')}}"></script>
+        <link rel="stylesheet" href="{{secure_asset('site/style.css')}}">
+        <script src="{{secure_asset('site/jquery.js')}}"></script>
+        <script src="{{secure_asset('site/bootstrap.js')}}"></script>
         <title>{{config('app.name', 'Portfolio')}}</title>
     </head>
     <body>
