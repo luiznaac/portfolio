cde6732503cd6e2acaa6c1f5b343893506a90be9
luiznaac <luizagnern@hotmail.com>
2020-07-11 12:54:56 -0300

    Adding dashboard overview

---
+++ app/Portfolio/Dashboard/Dashboard.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Portfolio\Dashboard;
+
+use App\Model\Stock\Position\StockPosition;
+use App\Model\Stock\Stock;
+
+class Dashboard {
+
+    private const PERCENTAGE_PRECISION = 2;
+
+    public static function getData(): array {
+        return [
+            'stock_positions_by_type' => self::getStockPositionsAndCalculatePercentages(),
+        ];
+    }
+
+    private static function getStockPositionsAndCalculatePercentages(): array {
+        $stock_positions_by_type = self::getLatestStockPositionsByType();
+        $total_amount = self::calculateTotalAmount($stock_positions_by_type);
+
+        return self::calculatePercentages($stock_positions_by_type, $total_amount);
+    }
+
+    private static function getLatestStockPositionsByType(): array {
+        $stock_positions = StockPosition::getLastStockPositions();
+
+        $positions_by_type = [];
+        /** @var StockPosition $stock_position */
+        foreach ($stock_positions as $stock_position) {
+            $stock_type_id = Stock::find($stock_position->stock_id)->stock_type_id;
+            $positions_by_type[$stock_type_id]['positions'][] = [
+                'position' => $stock_position,
+            ];
+        }
+
+        return $positions_by_type;
+    }
+
+    private static function calculateTotalAmount(array $stock_positions_by_type): float {
+        $amount = 0.00;
+        foreach ($stock_positions_by_type as $stock_type) {
+            foreach ($stock_type['positions'] as $stock_position) {
+                $amount += $stock_position['position']->amount;
+            }
+        }
+
+        return $amount;
+    }
+
+    private static function calculatePercentages(array $stock_positions_by_type, float $total_amount): array {
+        foreach ($stock_positions_by_type as &$stock_type) {
+            foreach ($stock_type['positions'] as &$stock_position) {
+                $stock_position['percentage'] =
+                    round(($stock_position['position']->amount/$total_amount)*100, self::PERCENTAGE_PRECISION);
+            }
+        }
+
+        foreach ($stock_positions_by_type as &$stock_type) {
+            $stock_type['percentage'] =
+                round(array_sum(array_column($stock_type['positions'], 'percentage')), self::PERCENTAGE_PRECISION);
+        }
+
+        return $stock_positions_by_type;
+    }
+}
---
+++ tests/Portfolio/Dashboard/DashboardTest.php
@@ -0,0 +1,114 @@
+<?php
+
+namespace Tests\Model\Stock\Position;
+
+use App\Model\Stock\Position\StockPosition;
+use App\Model\Stock\Stock;
+use App\Portfolio\Dashboard\Dashboard;
+use Carbon\Carbon;
+use Tests\TestCase;
+
+class DashboardTest extends TestCase {
+
+    private $user;
+
+    protected function setUp(): void {
+        parent::setUp();
+        $this->user = $this->loginWithFakeUser();
+
+        $this->createStockPosition(
+            Stock::getStockBySymbol('BOVA11'),
+            Carbon::parse('2020-06-30')
+        );
+
+        $this->createStockPosition(
+            Stock::getStockBySymbol('SQIA3'),
+            Carbon::parse('2020-07-01')
+        );
+
+        $this->user = $this->loginWithFakeUser();
+    }
+
+    public function testGetStockData(): void {
+        $stock_positions = $this->prepareScenario();
+        $stock_positions_by_type = $this->generateExpectedDataForStockPositions($stock_positions);
+
+        $data = Dashboard::getData();
+
+        $this->assertEquals($stock_positions_by_type, $data['stock_positions_by_type']);
+    }
+
+    private function generateExpectedDataForStockPositions(array $stock_positions): array {
+        $stock_positions_by_type = [];
+        /** @var StockPosition $stock_position */
+        foreach ($stock_positions as $stock_position) {
+            $stock = Stock::find($stock_position->stock_id);
+            $stock_positions_by_type[$stock->stock_type_id]['positions'][] = [
+                'position' => $stock_position,
+                'percentage' => 33.33
+            ];
+            $stock_positions_by_type[$stock->stock_type_id]['percentage'] = 33.33;
+        }
+
+        return $stock_positions_by_type;
+    }
+
+    private function prepareScenario(): array {
+        $stock_1 = Stock::getStockBySymbol('BOVA11');
+        $stock_2 = Stock::getStockBySymbol('SQIA3');
+        $stock_3 = Stock::getStockBySymbol('XPML11');
+
+        $date_1 = Carbon::parse('2020-06-26');
+        $date_2 = Carbon::parse('2020-06-29');
+
+        $this->createStockPosition(
+            $stock_1,
+            $date_1
+        );
+
+        $stock_1_position_2 = $this->createStockPosition(
+            $stock_1,
+            $date_2
+        );
+
+        $this->createStockPosition(
+            $stock_2,
+            $date_1
+        );
+
+        $stock_2_position_2 = $this->createStockPosition(
+            $stock_2,
+            $date_2
+        );
+
+        $this->createStockPosition(
+            $stock_3,
+            $date_1
+        );
+
+        $stock_3_position_2 = $this->createStockPosition(
+            $stock_3,
+            $date_2
+        );
+
+        return [
+            StockPosition::find($stock_1_position_2->id),
+            StockPosition::find($stock_2_position_2->id),
+            StockPosition::find($stock_3_position_2->id)
+        ];
+    }
+
+    private function createStockPosition(Stock $stock, Carbon $date): StockPosition {
+        $position = new StockPosition();
+        $position->user_id = $this->user->id;
+        $position->stock_id = $stock->id;
+        $position->date = $date->toDateString();
+        $position->quantity = 10;
+        $position->amount = 10;
+        $position->contributed_amount = 10;
+        $position->average_price = 1;
+        $position->save();
+
+        return $position;
+    }
+}
