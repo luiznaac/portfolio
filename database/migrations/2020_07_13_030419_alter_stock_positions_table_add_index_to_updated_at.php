755d4abfe4187c32f1b62e6f927c3b0cdd3df4bc
luiznaac <luizagnern@hotmail.com>
2020-07-13 20:57:00 -0300

    Creating stock positions index on updated_at column

---
+++ database/migrations/2020_07_13_030419_alter_stock_positions_table_add_index_to_updated_at.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AlterStockPositionsTableAddIndexToUpdatedAt extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('stock_positions', function (Blueprint $table) {
+            $table->index('updated_at');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('stock_positions', function (Blueprint $table) {
+            $table->dropIndex('stock_positions_updated_at_index');
+        });
+    }
+}
