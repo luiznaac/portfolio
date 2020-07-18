2fd19e23a97b00cc9b7ce50e5883a6e3fb7ed9cb
luiznaac <luizagnern@hotmail.com>
2020-07-18 16:50:55 -0300

    Removing orders sequence

--- app/Model/Order/Order.php
+++ app/Model/Order/Order.php
@@ -14,7 +14,6 @@ use Illuminate\Database\Eloquent\Model;
  *
  * @property int $id
  * @property int $user_id
- * @property int $sequence
  * @property int $stock_id
  * @property string $date
  * @property string $type
@@ -25,6 +24,8 @@ use Illuminate\Database\Eloquent\Model;
  */
 
 class Order extends Model {
+
+    protected $fillable = ['user_id', 'stock_symbol', 'date', 'quantity', 'price', 'cost'];
 
     public function user() {
         return $this->belongsTo('App\User');
@@ -53,7 +54,7 @@ class Order extends Model {
         $this->price = $price;
         $this->cost = $cost;
         $this->average_price = $this->calculateAveragePrice();
-        $this->save(['should_increment_sequence' => true]);
+        $this->save();
     }
 
     public static function createOrder(
@@ -132,28 +133,18 @@ class Order extends Model {
         return self::getBaseQuery()
             ->where('stock_id', $stock->id)
             ->where('date', '<=', $date->toDateString())
-            ->orderBy('sequence')
+            ->orderBy('date')
             ->get();
-    }
-
-    public function save(array $options = []) {
-        if(isset($options['should_increment_sequence'])) {
-            $this->sequence = (self::getBaseQuery()->max('sequence') ?? 0) + 1;
-        }
-
-        return parent::save($options);
     }
 
     public function delete() {
         parent::delete();
-
-        $this->updatePrecedingOrdersSequence();
         self::touchLastOrder();
     }
 
     private static function touchLastOrder(): void {
         /** @var Order $last_order */
-        $last_order = self::getBaseQuery()->orderByDesc('id')->get()->first();
+        $last_order = self::getBaseQuery()->orderByDesc('date')->get()->first();
         if($last_order) {
             $last_order->touch();
         }
@@ -165,17 +156,6 @@ class Order extends Model {
         }
 
         return 1;
-    }
-
-    private function updatePrecedingOrdersSequence() {
-        $preceding_orders = self::getBaseQuery()
-            ->where('sequence', '>', $this->sequence)->orderBy('sequence')->get();
-
-        /** @var Order $order */
-        foreach ($preceding_orders as $order) {
-            $order->sequence = $order->sequence - 1;
-            $order->save();
-        }
     }
 
     private function calculateAveragePrice(): float {
