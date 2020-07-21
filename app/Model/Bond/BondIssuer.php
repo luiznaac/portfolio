3f213edbcf93fa181e7fad1221c855540346ddc9
luiznaac <luizagnern@hotmail.com>
2020-07-21 18:05:22 -0300

    Adding bond issuers table migration and model

---
+++ app/Model/Bond/BondIssuer.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Model\Bond;
+
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * App\Model\Bond\BondIssuer
+ *
+ * @property int $id
+ * @property string $name
+ */
+
+class BondIssuer extends Model {
+
+}
