75c0f33ce1724a2b064cf3fa58ad81c6b1c17d4b
Luiz Agner <luizagnern@hotmail.com>
2021-06-20 21:42:33 -0300

    Deciding whether to force HTTPS based on env

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -24,6 +24,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        URL::forceScheme('https');
+        if (getenv('IS_PRODUCTION')) {
+            URL::forceScheme('https');
+        }
     }
 }
